0f31a6f70bb9c51d6ddd34a175778a7a8c27ccae
irenecampetti2 <62712106+irenecampetti2@users.noreply.github.com>
2020-04-22 18:03:56 +0200

    Update R_code_rs.r

--- R_code_rs.r
+++ R_code_rs.r
@@ -2,3 +2,63 @@
 
 install.packages("raster")
 install.packages("RStoolbox")
+
+setwd("C:/lab/")
+library(raster)
+p224r63_2011 <- brick("p224r63_2011_masked.grd")
+
+plot(p224r63_2011)
+
+# B1: blue
+# B2: green
+# B3: red
+# B4: NIR
+
+cl <- colorRampPalette(c('black','grey','light grey'))(100) # 
+plot(p224r63_2011, col=cl)
+
+# multiframe of different plots
+par(mfrow=c(2,2))
+# B1: blue
+clb <- colorRampPalette(c('dark blue','blue','light blue'))(100)
+plot(p224r63_2011$B1_sre, col=clb)
+
+# Exercise: do the same with the green band
+
+clg <- colorRampPalette(c('dark green','green','light green'))(100)
+plot(p224r63_2011$B2_sre, col=clg)
+
+# B3: red
+clr <- colorRampPalette(c('dark red','red','pink'))(100)   # light red does not exist
+plot(p224r63_2011$B3_sre, col=clr)
+
+# B4: NIR
+cln <- colorRampPalette(c('red','orange','yellow'))(100)
+plot(p224r63_2011$B4_sre, col=cln)
+
+
+par(mfrow=c(4,1))
+# B1: blue
+clb <- colorRampPalette(c('dark blue','blue','light blue'))(100)
+plot(p224r63_2011$B1_sre, col=clb)
+
+# Exercise: do the same with the green band
+
+clg <- colorRampPalette(c('dark green','green','light green'))(100)
+plot(p224r63_2011$B2_sre, col=clg)
+
+# B3: red
+clr <- colorRampPalette(c('dark red','red','pink'))(100)   # light red does not exist
+plot(p224r63_2011$B3_sre, col=clr)
+
+# B4: NIR
+cln <- colorRampPalette(c('red','orange','yellow'))(100)
+plot(p224r63_2011$B4_sre, col=cln)
+
+
+# plotRGB
+plotRGB(p224r63_2011, r=3, g=2, b=1, stretch="Lin")
+plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="Lin")
+
+# Exercise: put NIR on top of the G component
+plotRGB(p224r63_2011, r=3, g=4, b=2, stretch="Lin")
