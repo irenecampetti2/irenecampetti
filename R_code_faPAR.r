d0bb39b9eda7ab8c7fac9f7078bdf731662d6bdb
irenecampetti2 <62712106+irenecampetti2@users.noreply.github.com>
2020-05-22 15:02:17 +0200

    Update R_code_faPAR.r

--- R_code_faPAR.r
+++ R_code_faPAR.r
@@ -21,3 +21,36 @@ dev.off()
 pdf("faPAR10.pdf")
 levelplot(faPAR10)
 dev.off()
+
+####
+#regression model between faPAR and NDVI
+# amount of heavy metals and erosion
+erosion <- c(12,14,16,24,26,40,55,67)
+hm <- c(30,100,150,200,260,340,460,600)
+
+plot(erosion, hm, col="red", pch=19, xlab="erosion", ylab="heavy metals", cex=2)
+model1 <- lm(hm ~ erosion)
+summary(model1)
+abline(model1)
+
+library(raster)
+library(rasterdiv)
+libray(sf)
+setwd("C:/lab/")
+raster("faPAR10.tif")
+
+plot(faPAR10)
+plot(copNDVI)
+
+copNDVI <- reclassify(copNDVI,cbind(253:255,NA),right=TRUE)
+random.points <- function(raster,n)
+{
+lin <- rasterToContour(is.na(raster))
+pol <- as(st_union(st_polygonize(st_as_sf(lin))), 'Spatial') # st_union to dissolve geometries
+pts <- spsample(pol[1,], n, type = 'random')
+}
+
+pts <- random.points(faPAR10,1000)
+copNDVIp <- extract(copNDVI, pts)
+faPAR10p <- extract(faPAR10,pts)
+
