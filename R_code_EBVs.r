996cb2cc140198769b9f8d6ba84dc30226e63ffe
irenecampetti2 <62712106+irenecampetti2@users.noreply.github.com>
2020-05-27 17:28:31 +0200

    Update R_code_EBVs.r

--- R_code_EBVs.r
+++ R_code_EBVs.r
@@ -39,3 +39,44 @@ plot(sd_snt, col=cl)
 par(mfrow=c(1,2))
 plotRGB(snt,4,3,2, stretch="lin", main="original image") 
 plot(sd_snt, col=cl, main="diversity")
+
+##########################
+
+# Cladonia stellaris example
+setwd("C:/lab/")
+
+# import the image, RGB layers
+library(raster)
+clad <- brick("cladonia_stellaris_calaita.JPG")
+plotRGB(clad,1,2,3,stretch="lin")
+
+# make a 3x3 matrix
+window <- matrix(1, nrow=3, ncol=3)
+window
+
+# calculate values for the neighborhood of focal cells --> focal()
+pairs(clad)
+
+# PCA
+library(RStoolbox)
+cladpca <- rasterPCA(clad)
+cladpca
+
+summary(cladpca$model)
+# 98%
+
+plotRGB(cladpca$map,1,2,3,stretch="lin")
+
+sd_clad <- focal(cladpca$map$PC1, w=window, fun=sd)
+
+# PC1 aggregate
+PC1_agg <- aggregate(cladpca$map$PC1, fact=10)
+sd_clad_agg <- focal(PC1_agg, w=window, fun=sd)
+
+# multiframe graph
+par(mfrow=c(1,2))
+cl <- colorRampPalette(c('yellow','violet','black'))(100) #
+plot(sd_clad, col=cl)
+plot(sd_clad_agg, col=cl)
+
+
