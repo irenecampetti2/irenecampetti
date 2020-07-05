a423ce3748403fb2c539b9eb77321bd0223ccd21
irenecampetti2 <62712106+irenecampetti2@users.noreply.github.com>
2020-07-05 16:36:20 +0200

    Update R_code_exam.r

--- R_code_exam.r
+++ R_code_exam.r
@@ -48,6 +48,8 @@ attach(meuse)
 # Exercise: see the names of the variables and plot cadmium versus zinc
 # There are two ways to se the names of the variables:
 head(meuse) # shows first 6 lines of the dataset
+# or View(meuse)
+# or meuse
 plot(cadmium,zinc)
 
 # add a change in symbol, color, size
@@ -108,6 +110,7 @@ bubble(meuse, "copper", main="Copper concentration", col="red")
 setwd("C:/lab/")
 
 # import the dataset and read it in a table format
+# it is possible to name the dataset by --> name <-
 covid <- read.table("covid_agg.csv", head=T)
 
 head(covid)
@@ -154,28 +157,165 @@ ggplot(covid, aes(x=lon, y=lat, size=cases)) + geom_point()
 ##################################
 ##################################
 
-# R code for multivariate analysis
+# 4. R code for multivariate analysis
 
+# Install vegan package for vegetation analysis, then require it
+install.packages("vegan")
 library(vegan)
+
+# Set working directory for Windows
 setwd("C:/lab/")
 
-biomes <- read.table("biomes.csv", header=T, sep=",")
-head(biomes) # view(biomes), biomes are also good
+# Read the dataset and name it biomes
+biomes <- read.table("biomes.csv", header=T, sep=",") # header=T tells R that the first row contains the names of the variables of the table
+# sep is the separator character
 
-# DEtrended CORrespondence ANAlysis
+head(biomes) # View(biomes), biomes are also good
+
+# DEtrended CORrespondence ANAlysis decorana
 multivar <- decorana(biomes) 
-plot(multivar)
 multivar
-
 plot(multivar)
+
+# Biomes types
 biomes_types <- read.table("biomes_types.csv", header=T, sep=",")
 head(biomes_types)
-
 attach(biomes_types)
-ordiellipse(multivar, type, col=1:4, kind = "ehull", lwd=3)
 
-# Also col="blue", "red", "green", "black")
+ordiellipse(multivar, type, col=1:4, kind = "ehull", lwd=3) # Also col="blue", "red", "green", "black")
+# draws lines or polygons for dispersion
 
-ordispider(multivar, type, col=1:4, label= TRUE)
+ordispider(multivar, type, col=1:4, label= TRUE) # draws a spider diagram in which each point is connected to the group centroid
 
+##################################
+##################################
+
+# 5. R code for remote sensing data analysis
+
+# install the packages raster and RStoolbox
+install.packages("raster")
+install.packages("RStoolbox")
+
+setwd("C:/lab/")
+
+library(raster)
+
+# Create a multi-layer raster object using brick() from a multi-layer file
+p224r63_2011 <- brick("p224r63_2011_masked.grd")
+
+plot(p224r63_2011)
+
+# B1: blue
+# B2: green
+# B3: red
+# B4: NIR
+
+# Create a different color palette and name it cl
+cl <- colorRampPalette(c('black','grey','light grey'))(100) # (100) is for the saturation of colors 
+plot(p224r63_2011, col=cl)
+
+# multiframe of different plots
+par(mfrow=c(2,2))
+# B1: blue
+clb <- colorRampPalette(c('dark blue','blue','light blue'))(100)
+plot(p224r63_2011$B1_sre, col=clb)
+
+# Exercise: do the same with the green band
+
+clg <- colorRampPalette(c('dark green','green','light green'))(100)
+plot(p224r63_2011$B2_sre, col=clg)
+
+# B3: red
+clr <- colorRampPalette(c('dark red','red','pink'))(100)   # light red does not exist
+plot(p224r63_2011$B3_sre, col=clr)
+
+# B4: NIR
+cln <- colorRampPalette(c('red','orange','yellow'))(100)
+plot(p224r63_2011$B4_sre, col=cln)
+
+
+par(mfrow=c(4,1))
+# B1: blue
+clb <- colorRampPalette(c('dark blue','blue','light blue'))(100)
+plot(p224r63_2011$B1_sre, col=clb)
+
+# Exercise: do the same with the green band
+
+clg <- colorRampPalette(c('dark green','green','light green'))(100)
+plot(p224r63_2011$B2_sre, col=clg)
+
+# B3: red
+clr <- colorRampPalette(c('dark red','red','pink'))(100)   # light red does not exist
+plot(p224r63_2011$B3_sre, col=clr)
+
+# B4: NIR
+cln <- colorRampPalette(c('red','orange','yellow'))(100)
+plot(p224r63_2011$B4_sre, col=cln)
+
+
+# plotRGB
+plotRGB(p224r63_2011, r=3, g=2, b=1, stretch="Lin")
+plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="Lin")
+
+# Exercise: put NIR on top of the G component
+plotRGB(p224r63_2011, r=3, g=4, b=2, stretch="Lin")
+
+#######
+
+setwd("C:/lab/")
+load("rs.RData")
+ls()
+
+library(raster)
+p224r63_1988 <- brick("p224r63_1988_masked.grd")
+
+plot(p224r63_1988)
+
+# Exercise: plot in visible RGB 321 both images
+p224r63_2011 <- brick("p224r63_2011_masked.grd")
+par(mfrow=c(2,1))
+plotRGB(p224r63_1988, r=3, g=2, b=1, stretch="Lin")
+plotRGB(p224r63_2011, r=3, g=2, b=1, stretch="Lin")
+
+# Exercise: plot in false colour 432 both images
+par(mfrow=c(2,1))
+plotRGB(p224r63_1988, r=4, g=3, b=2, stretch="Lin")
+plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="Lin")
+
+# Enhance the noise
+par(mfrow=c(2,1))
+plotRGB(p224r63_1988, r=4, g=3, b=2, stretch="Hist")
+plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="Hist")
+
+
+# B1: blue
+# B2: green
+# B3: red
+# B4: NIR
+dvi2011 <- p224r63_2011$B4_sre - p224r63_2011$B3_sre
+cl <- colorRampPalette(c('darkorchid3','light blue','lightpink4'))(100)
+plot(dvi2011, col=cl)
+
+# Exercise: dvi for 1988
+dvi1988 <- p224r63_1988$B4_sre - p224r63_1988$B3_sre
+clb <- colorRampPalette(c('darkorchid3','light blue','lightpink4'))(100)
+plot(dvi1988, col=clb)
+
+# Difference from one year to another
+diff <- dvi2011 - dvi1988
+plot(diff)
+
+# Changing grain
+p224r63_2011res <- aggregate(p224r63_2011, fact=10)
+p224r63_2011res100 <- aggregate(p224r63_2011, fact=100)
+
+par(mfrow=c(3,1))
+plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="Lin")
+plotRGB(p224r63_2011res, r=4, g=3, b=2, stretch="Lin")
+plotRGB(p224r63_2011res100, r=4, g=3, b=2, stretch="Lin")
+
+p224r63_2011
+p224r63_2011res
+p224r63_2011res100
+  
 
