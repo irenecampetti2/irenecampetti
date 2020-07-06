2319fe30dbd9693991ae6a0633e27d55f2985818
irenecampetti2 <62712106+irenecampetti2@users.noreply.github.com>
2020-07-06 13:43:12 +0200

    Update R_code_exam.r

--- R_code_exam.r
+++ R_code_exam.r
@@ -157,7 +157,101 @@ ggplot(covid, aes(x=lon, y=lat, size=cases)) + geom_point()
 ##################################
 ##################################
 
-# 4. R code for multivariate analysis
+# 4. R code for point pattern analysis: density map
+
+setwd("C:/lab/")
+
+# Install spatstat package and require it
+install.packages("spatstat")
+library(spatstat)
+
+# Import data, head=T means that there is a column header
+covid <- read.table("covid_agg.csv",head=T)
+
+attach(covid)
+head(covid)
+
+# Point pattern dataset represented in two dimensions
+# The vectors represent longitude and latitude
+covids <- ppp(lon, lat, c(-180, 180), c(-90, 90)) # if not using attach() --> ppp(covid$lon, covid$lat, c(-180,180), c(-90,90))
+
+# Estimate density
+d <- density(covids)
+plot(d)
+
+# Show points at specific coordinates
+points(covids)
+
+setwd("C:/lab/")
+
+# Load previous workspace
+load(".RData")
+ls()
+
+# covids: point pattern
+# d: density map
+
+library(spatstat)
+plot(d)
+points(covids)
+
+# Install and require rgdal package
+install.packages("rgdal")
+library(rgdal)
+
+# Let's input vector lines (x0y0, x1y1, x2,y2...)
+# Import coastlines
+coastlines <- readOGR("ne_10m_coastline.shp")
+
+# Add coastlines to the density map
+plot(d)
+points(covids)
+plot(coastlines, add=T) # add=T adds to current plot
+
+# Change the color of the graph
+cl <- colorRampPalette(c("yellow", "orange", "red")) (100)
+plot(d, col=cl)
+points(covids)
+plot(coastlines, add=T)
+
+# Exercise: make a colorRampPalette
+clr <- colorRampPalette(c("light green", "yellow","orange","violet")) (100)
+plot(d, col=clr, main="Densities of covid-19") # main adds a title of the graph
+points(covids)
+plot(coastlines, add=T)
+
+# New example: abrupt change of colors!
+cll <- colorRampPalette(c("light green", "yellow","orange","violet")) (5) # lower number of intermediate colors
+plot(d, col=cll, main="Densities of covid-19")
+points(covids)
+plot(coastlines, add=T)
+
+# Use a higher number of intermediate colors
+clr <- colorRampPalette(c("light green", "yellow","orange","violet")) (1000) 
+plot(d, col=clr, main="Densities of covid-19")
+points(covids)
+plot(coastlines, add=T)
+
+# Export as .pdf
+pdf("covid_density.pdf")
+cl <- colorRampPalette(c("light green", "yellow","orange","violet")) (100)
+plot(d, col=cl, main="Densities of covid-19")
+points(covids)
+plot(coastlines, add=T)
+dev.off() # closes the current plot
+
+# Export as .png
+png("covid_density.png")
+clr <- colorRampPalette(c("light green", "yellow","orange","violet")) (100)
+plot(d, col=clr, main="Densities of covid-19")
+points(covids)
+plot(coastlines, add=T)
+dev.off()
+
+##################################
+##################################
+
+# 5. R code for multivariate analysis
 
 # Install vegan package for vegetation analysis, then require it
 install.packages("vegan")
@@ -190,7 +284,7 @@ ordispider(multivar, type, col=1:4, label= TRUE) # draws a spider diagram in whi
 ##################################
 ##################################
 
-# 5. R code for remote sensing data analysis
+# 6. R code for remote sensing data analysis
 
 # install the packages raster and RStoolbox
 install.packages("raster")
@@ -316,5 +410,5 @@ p224r63_2011res100
 ##################################
 ##################################
 
-# 6. R code 
+# 7. R code 
 
